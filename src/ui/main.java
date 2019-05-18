6805134aca2cb070b71910c9f801e28c4948925f
roychen7 <48664002+roychen7@users.noreply.github.com>
2019-05-17 23:00:06 -0700

    Add files via upload

---
+++ src/ui/main.java
@@ -0,0 +1,16 @@
+package ui;
+
+import javax.swing.*;
+import java.io.IOException;
+
+public class main extends JFrame{
+
+    public static void main(String[] args) throws IOException {
+
+        login login = new login();
+        login.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        login.setSize(1100, 200);
+        login.setLocationRelativeTo(null);
+        login.setVisible(true);
+    }
+}
